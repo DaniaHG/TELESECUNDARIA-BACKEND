bafa144aa41199b9cff228833d5916fd8b8ae4cd
DaniaHG21 <dhernandez@energetika.cl>
2022-11-05 08:49:20 -0600

    make it better

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./configurations/route');
 const auth = require("./security/verifier");
-const morgan = require('morgan');
+
 
 const app = express();
-if (process.env.NODE_ENV !== 'production') {
-    app.use(morgan('common'))
-}
+
 
 app.use(express.json());
 
